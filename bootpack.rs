1aba8a33f3834cdfe0968abff8435c70e60d21e9
penguinshunya <penguinshunya@hotmail.com>
2017-12-20 00:47:29 +0900

    init_screen関数内のコメントアウトを外すと、`memset`未定義エラー

--- bootpack.rs
+++ bootpack.rs
@@ -9,9 +9,19 @@ extern {
     fn io_store_eflags(eflags: u32);
 }
 
+#[repr(C, packed)]
+struct BOOTINFO {
+    cyls: u8, leds: u8, vmode: u8, reserve: u8,
+    scrnx: u16, scrny: u16,
+    vram: *mut u8,
+}
+
 #[no_mangle]
 pub unsafe fn hari_main() {
+    let binfo = 0x00000ff0 as *const BOOTINFO;
+
     init_palette();
+    init_screen((*binfo).vram, (*binfo).scrnx as u32, (*binfo).scrny as u32);
 
     loop {
         asm!("hlt");
@@ -38,20 +48,62 @@ const TABLE_RGB: [u32; 48] = [
 ];
 
 unsafe fn init_palette() {
-	set_palette(0, 15, &TABLE_RGB);
+	set_palette(&TABLE_RGB);
 }
 
-unsafe fn set_palette(start: u32, end: u32, rgb: &[u32; 48]) {
+unsafe fn set_palette(rgb: &[u32; 48]) {
     let eflags = io_load_eflags();
     io_cli();
-    io_out8(0x03c8, start);
-    // for i in start..(end + 1) {
-    //     let n = (i / 3 * 3 + i) as usize;
-    //     io_out8(0x03c9, rgb[n]);
-    //     io_out8(0x03c9, rgb[n + 1]);
-    //     io_out8(0x03c9, rgb[n + 2]);
-    // }
+    io_out8(0x03c8, 0);
+    for i in 0..48 {
+        io_out8(0x03c9, rgb[i]);
+    }
     io_store_eflags(eflags);
+}
+
+const COL8_000000: u8 = 0;
+const COL8_FF0000: u8 = 1;
+const COL8_00FF00: u8 = 2;
+const COL8_FFFF00: u8 = 3;
+const COL8_0000FF: u8 = 4;
+const COL8_FF00FF: u8 = 5;
+const COL8_00FFFF: u8 = 6;
+const COL8_FFFFFF: u8 = 7;
+const COL8_C6C6C6: u8 = 8;
+const COL8_840000: u8 = 9;
+const COL8_008400: u8 = 10;
+const COL8_848400: u8 = 11;
+const COL8_000084: u8 = 12;
+const COL8_840084: u8 = 13;
+const COL8_4488CC: u8 = 14;
+const COL8_848484: u8 = 15;
+
+unsafe fn init_screen(vram: *mut u8, xsize: u32, ysize: u32) {
+    // boxfill8(vram, xsize, COL8_4488CC,          0,          0, xsize -  1, ysize - 29);
+    // boxfill8(vram, xsize, COL8_C6C6C6,          0, ysize - 28, xsize -  1, ysize - 28);
+    // boxfill8(vram, xsize, COL8_FFFFFF,          0, ysize - 27, xsize -  1, ysize - 27);
+    // boxfill8(vram, xsize, COL8_C6C6C6,          0, ysize - 26, xsize -  1, ysize -  1);
+
+    boxfill8(vram, xsize, COL8_FFFFFF,          3, ysize - 24,         59, ysize - 24);
+    boxfill8(vram, xsize, COL8_FFFFFF,          2, ysize - 24,          2, ysize -  4);
+    boxfill8(vram, xsize, COL8_848484,          3, ysize -  4,         59, ysize -  4);
+    boxfill8(vram, xsize, COL8_848484,         59, ysize - 23,         59, ysize -  5);
+    boxfill8(vram, xsize, COL8_000000,          2, ysize -  3,         59, ysize -  3);
+    boxfill8(vram, xsize, COL8_000000,         60, ysize - 24,         60, ysize -  3);
+
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
+    boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
+    boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+}
+
+unsafe fn boxfill8(vram: *mut u8, xsize: u32, c: u8, x0: u32, y0: u32, x1: u32, y1: u32)
+{
+    for y in y0..(y1 + 1) {
+        for x in x0..(x1 + 1) {
+            *vram.offset((y * xsize + x) as isize) = c;
+        }
+    }
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
